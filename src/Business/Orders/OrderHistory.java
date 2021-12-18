781a2e437f0e5dd188d7ff7d038eebfb30110c20
bismita15 <74596798+Bismita15@users.noreply.github.com>
2021-12-17 23:25:47 -0500

    HotelManagement
    Hotel Management Full Project Initial Commit

---
+++ src/Business/Orders/OrderHistory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Orders;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bhabani
+ */
+public class OrderHistory {
+    
+     ArrayList<Order> orderHisotry;
+
+    public ArrayList<Order> getOrderHisotry() {
+        return orderHisotry;
+    }
+
+    public void setOrderHisotry(ArrayList<Order> orderHisotry) {
+        this.orderHisotry = orderHisotry;
+    }
+    
+    
+}
